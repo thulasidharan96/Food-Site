60009da1b24bc24afd8a7524fa5b7b73e976e866
Thulasidharan96 <thulasidharankvp@gmail.com>
2025-01-22 15:15:53 +0530

    Optimized For Smooth Expirence

--- src/components/Layouts/Footer.js
+++ src/components/Layouts/Footer.js
@@ -1,9 +1,119 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
 
-export default Footer
+  const [isVisible, setIsVisible] = useState(false);
+  const scrollTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  const listenToScroll = () => {
+    let heightToHidden = 250;
+    const windowScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+
+    windowScroll > heightToHidden ? setIsVisible(true) : setIsVisible(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", listenToScroll);
+  });
+
+  return (
+    <>
+      <footer>
+        <Container>
+          <Row>
+            <Col sm={6} lg={3} className="mb-4 mb-lg-0">
+              <div className="text-center">
+                <h5>Location</h5>
+                <p>Kovilpatti</p>
+                <p>TamilNadu</p>
+              </div>
+            </Col>
+            <Col sm={6} lg={3} className="mb-4 mb-lg-0">
+              <div className="text-center">
+                <h5>Location</h5>
+                <p>Kovilpatti</p>
+                <p>TamilNadu</p>
+              </div>
+            </Col>
+            <Col sm={6} lg={3} className="mb-4 mb-lg-0">
+              <div className="text-center">
+                <h5>Order Now</h5>
+                <p>
+                  <Link to="tel:1234567890" className="call">
+                    1234567890
+                  </Link>
+                </p>
+              </div>
+            </Col>
+            <Col sm={6} lg={3} className="mb-4 mb-lg-0">
+              <div className="text-center">
+                <h5>Follow Us</h5>
+                <p>Quaerat neque purus ipsum</p>
+                <ul className="list-unstyled text-center mt-2">
+                  <li>
+                    <Link to="/">
+                      <i class="bi bi-facebook"></i>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/">
+                      <i class="bi bi-twitter"></i>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/">
+                      <i class="bi bi-instagram"></i>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/">
+                      <i class="bi bi-youtube"></i>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </Col>
+          </Row>
+          <Row className="copy_right">
+            <Col>
+              <div>
+                <ul className="list-unstyled text-center mb-0">
+                  <li>
+                    <Link to="/">
+                      © 2025 <span>XYZ</span>. All Rights Reserved
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/">About Us</Link>
+                  </li>
+                  <li>
+                    <Link to="/">Terms Of Use</Link>
+                  </li>
+                  <li>
+                    <Link to="/">Privacy Policy</Link>
+                  </li>
+                </ul>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
+      {/* Sroll To Top */}
+      {isVisible && (
+        <div className="scroll_top" onClick={scrollTop}>
+          <i class="bi bi-arrow-up"></i>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Footer;
